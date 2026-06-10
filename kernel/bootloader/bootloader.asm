310c11867c908805fe0099d4821a1c23a03ff366
lucas-rabello-dev <lucasrabello2020@gmail.com>
2026-06-10 17:49:44 -0300

    feat: add read_disk label in bootloader.asm

--- kernel/bootloader/bootloader.asm
+++ kernel/bootloader/bootloader.asm
@@ -4,7 +4,19 @@ org 0x7C00
 start:
 	mov si, hello
 	call print_string
+	jmp read_disk
 	jmp loop
+
+
+read_disk:
+	; what disk do we want to read? (diskNum), for example the 'n1' (number 1) of /dev/nvme0n1p1
+	; CHS address: C -> Cylinder of the disk, H -> head, S -> Sector of the disk
+	; to load the disk in RAM
+
+	ret	
+
+
+
 
 print_string:
 	;enter in teletype mode and print 'a' char
