4311cd5d8b5a1c88a178e77df216181af159e510
lucas-rabello-dev <lucasrabello2020@gmail.com>
2026-06-11 08:08:16 -0300

    feat: updates

--- kernel/bootloader/bootloader.asm
+++ kernel/bootloader/bootloader.asm
@@ -2,9 +2,27 @@ bits 16
 org 0x7C00
 
 start:
+	
+	; for security all this registers equals to 0
+	xor ax, ax
+	mov ds, ax
+	mov es, ax
+	mov sp, 0x7A00 ; Stack Pointer
+
 	mov si, hello
 	call print_string
-	jmp read_disk
+	call read_disk
+	; A20
+	; to acess 1MB of memory (protect mode need this)
+	mov al, 0x92 ; fast A20, le o controlador rapido do A20
+	or al, 00000010b ; liga o bit do A20
+	out 0x92, al ; envia de volta -> A20 habilitado
+
+	; GDT
+	
+
+
+	; jmp 0x1000 ; jump to address of the kernel
 	jmp loop
 
 
@@ -12,11 +30,15 @@ read_disk:
 	; what disk do we want to read? (diskNum), for example the 'n1' (number 1) of /dev/nvme0n1p1
 	; CHS address: C -> Cylinder of the disk, H -> head, S -> Sector of the disk
 	; to load the disk in RAM
-
+	mov ah, 0x02 ; for read the disk
+	mov al, 1 ; quantity of sectors
+	mov ch, 0 ; cylinder
+	mov cl, 2 ; second sector of the disk
+	mov dh, 0 ; head
+	mov dl, 0x80 ; drive (its a HD or something else)
+	mov bx, 0x1000 ; address where the kernel will start in RAM
+	int 0x13
 	ret	
-
-
-
 
 print_string:
 	;enter in teletype mode and print 'a' char
